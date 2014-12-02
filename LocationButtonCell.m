d5f22856b70ccbff80a2806355ae2c578ed4d0f9
Sergio Perez <spinfinite07@gmail.com>
2014-12-01 19:16:43 -0700

    Displays Lat Longs in Command Line
    Changes were made to call a function dedicated to set and display the lat longs.

--- LocationButtonCell.m
+++ LocationButtonCell.m
@@ -12,9 +12,11 @@
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
+
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
+        
         [self setup];
     }
     return self;
@@ -31,31 +33,31 @@
 }
 
 -(void)setup{
+    [self setupLocationManager];
     
     UIButton *getCurrentButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [getCurrentButton setTitle:@"Push to get Location" forState:UIControlStateNormal];
-    [getCurrentButton addTarget:self action:@selector(locationMethod) forControlEvents:UIControlEventTouchUpInside];
+    
+    [getCurrentButton addTarget:self.locationManager action:@selector(startUpdatingLocation) forControlEvents:UIControlEventTouchUpInside];
     
     getCurrentButton.frame = CGRectMake(0.0, 0.0, self.frame.size.width, self.frame.size.height);
     getCurrentButton.backgroundColor = [UIColor greenColor];
     [self addSubview:getCurrentButton];
     
-
-    
 }
 
--(void)locationMethod{
-    
-    if (self.locationManager == nil) {
-        self.locationManager = [[CLLocationManager alloc] init];
-    }
+- (void)setupLocationManager {
+    self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
     self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     self.locationManager.distanceFilter = 10.0;
-//    [self.locationManager startUpdatingLocation];
-    [self.locationManager startMonitoringSignificantLocationChanges];
-    
-    CLLocation* location = [CLLocation new];
+    [self.locationManager requestWhenInUseAuthorization];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation *location = locations.firstObject;
+
     NSDate* eventDate = location.timestamp;
     NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
     if (abs(howRecent) < 5.0) {
@@ -63,48 +65,26 @@
         NSLog(@"latitude %+.6f, longitude %+.6f\n",
               location.coordinate.latitude,
               location.coordinate.longitude);
-        
-//        NSLog(@"%@", @"You Are Pushing The Button");
     }
-//    NSLog(@"%@", @"You Are Pushing The Button");
-    
 }
 
-//- (void)startSignificantChangeUpdates
-//{
-//    CLLocationManager *locationManager = [[CLLocationManager alloc] init];
-//    // Create the location manager if this object does not
-//    // already have one.
-//    if (nil == locationManager)
-//        locationManager = [[CLLocationManager alloc] init];
-//    
-//    locationManager.delegate = self;
-//    [locationManager startMonitoringSignificantLocationChanges];
-//    
-//}
+- (void)startSignificantChangeUpdates
+{
+    CLLocationManager *locationManager = [[CLLocationManager alloc] init];
+    // Create the location manager if this object does not
+    // already have one.
+    if (nil == locationManager)
+        self.locationManager = [[CLLocationManager alloc] init];
+
+    [self.locationManager startMonitoringSignificantLocationChanges];
+    
+}
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
     
     NSLog(@"Error: %@", error);
     NSLog(@"Failed to get location");
     
-}
-
-
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
-    
-    // If it's a relatively recent event, turn off updates to save power.
-    CLLocation* location = [locations lastObject];
-    NSDate* eventDate = location.timestamp;
-    NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
-    if (abs(howRecent) < 5.0) {
-        // If the event is recent, do something with it.
-        NSLog(@"latitude %+.6f, longitude %+.6f\n",
-              location.coordinate.latitude,
-              location.coordinate.longitude);
-        
-             NSLog(@"%@", @"You Are Pushing The Button");
-    }
 }
 
 @end
